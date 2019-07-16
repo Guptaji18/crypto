edc497e2d7b268a1a74409b1372ee0fc6e66dc47
Guptaji18 <34456844+Guptaji18@users.noreply.github.com>
2019-07-16 23:09:06 +0530

    Create d_Affine.py

---
+++ decipher/d_Affine.py
@@ -0,0 +1,25 @@
+
+def egcd(a, b): 
+	x,y, u,v = 0,1, 1,0
+	while a != 0: 
+		q, r = b//a, b%a 
+		m, n = x-u*q, y-v*q 
+		b,a, x,y, u,v = a,r, u,v, m,n 
+	gcd = b 
+	return gcd, x, y 
+
+def modinv(a, m): 
+	gcd, x, y = egcd(a, m) 
+	if gcd != 1: 
+		return None 
+	else: 
+		return x % m 
+
+
+
+
+def affine_decrypt(cipher, key): 
+
+	return ''.join([ chr((( modinv(key[0], 26)*(ord(c) - ord('A') - key[1])) 
+					% 26) + ord('A')) for c in cipher ]) 
+
